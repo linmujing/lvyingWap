d12127e9d3ed1f068e1023e2bdc8a5ac340f37fd
MTmatao <1414294651@qq.com>
2018-09-11 19:16:34 +0800

    no message

--- 项目代码/lvyingWEB/src/router/index.js
+++ 项目代码/lvyingWEB/src/router/index.js
@@ -4,12 +4,34 @@ import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '/',
+      meta:{
+        title:'首页',
+      }
+    },
+    {
+      path: '*',
+      name: '404',
+      meta:{
+        title:'404',
+      },
+      component: resolve => require(['@/view/404'],resolve)
     }
-  ]
+  	
+  ],
+  mode: 'history'
 })
+
+router.beforeEach((to,form,next) =>{ 
+  /*路由变化修改title*/ 
+  if(to.meta.title){
+    document.title=to.meta.title;
+  } 
+  next(); 
+})
+export default router
